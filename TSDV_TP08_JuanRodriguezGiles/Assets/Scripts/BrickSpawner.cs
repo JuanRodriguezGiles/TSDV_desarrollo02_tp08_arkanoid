76ade67dfe7b43e8e20c338b50e64dbf4ebfad54
Juan <juanrodriguezgiles@gmail.com>
2021-05-28 19:47:57 -0300

    Implemented brick spawner

--- TSDV_TP08_JuanRodriguezGiles/Assets/Scripts/BrickSpawner.cs
+++ TSDV_TP08_JuanRodriguezGiles/Assets/Scripts/BrickSpawner.cs
@@ -1,17 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 public class BrickSpawner : MonoBehaviour
 {
     public GameObject BrickGameObject;
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        const int rows = 5;
+        const int columns = 10;
+        float offsetX = 2.5f;
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Vector3 positionVector3 = new Vector3(j * offsetX, i, 0);
+                Instantiate(BrickGameObject, positionVector3, Quaternion.identity);
+            }
+        }
     }
 }
